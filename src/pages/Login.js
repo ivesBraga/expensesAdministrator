721dff41ecc33ede6ef9e54432cb1a586cdb3694
Ives <ivesmoraisbraga@gmail.com>
2022-02-09 15:46:17 -0300

    abrindo a pr

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <Redirect to="/" />
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="***"
+        />
+
+        <button
+          type="button"
+          disabled="true"
+        >
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
